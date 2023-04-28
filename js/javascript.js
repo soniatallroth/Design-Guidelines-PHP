12fd3d0825e751e13523e2e2c486232d8cec700a
stallroth <sonia.tallroth@gmail.com>
2023-04-28 16:43:26 -0500

    added dropdowns in navbar, changed some colors

--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,21 @@
+// dropdown js
+/* Loop through all dropdown buttons to toggle between hiding and showing its dropdown content - This allows the user to have multiple dropdowns without any conflict */
+var dropdown = document.getElementsByClassName("dropdown");
+var i;
+
+for (i = 0; i < dropdown.length; i++) {
+  dropdown[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var dropdownContent = this.nextElementSibling;
+    if (dropdownContent.style.display === "block") {
+      dropdownContent.style.display = "none";
+    } else {
+      dropdownContent.style.display = "block";
+    }
+  });
+}
+
+// code snippets
 $(document).ready(function() {
 	$('pre').makeCode({style: 'code-style-dark', addons: [{ name: 'js', parser: javascriptCode }]});
 });
@@ -5,8 +23,6 @@ $(document).ready(function() {
 var javascriptCode = function(text) {
 	return text;
 };
-
-// All shit
 
 var csharpCode = function(text) {
 	var comment = false;
